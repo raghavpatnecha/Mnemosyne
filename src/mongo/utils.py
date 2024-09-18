dae809760462fe54bfde4cb5fb65bdcf0b6d4ff3
Akshay Bahadur <akshaybahadur21@gmail.com>
2024-09-18 05:26:37 +0000

    minor commit

--- src/mongo/utils.py
+++ src/mongo/utils.py
@@ -2,7 +2,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-def extract_data_from_url(url: str) -> dict:
+def extract_data_from_url(url: str):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
